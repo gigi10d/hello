ed666e28fb1208b1c834cdcca42f5112a42e80fc
gigi10d <giovannaoliveirarodrigues17@gmail.com>
2024-04-18 10:53:39 -0300

    kivy

--- hello.py
+++ hello.py
@@ -3,5 +3,5 @@ from kivy.uix.button import Button
 class MyApp(App):
     def build(self):
         return Button(text= "Hello World! This is my first program in kivy \n I'm a SESIANO Student, and I love my Teacher")
-if __name__ == '_name_':
+if __name__ == '__main__':
     MyApp().run() 
